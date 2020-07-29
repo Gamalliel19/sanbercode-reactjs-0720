8175c58e0d374699bc425aef461d616dd2c1a86f
Gamalliel19 <sharongamalliel@gmail.com>
2020-07-29 13:46:07 +0700

    Menambahkan tugas hari ke 7 javascript

--- Pekan 2/Tugas-Hari-Ke-2/index2.js
+++ Pekan 2/Tugas-Hari-Ke-2/index2.js
@@ -9,23 +9,43 @@ var books = [
 // Lanjutkan code untuk menjalankan function readBooksPromise 
 
 
+var bookAntri = books.length;
+var time = 10000;
 
-function bacaBuku(){
-    readBooksPromise(10000, books[0].then(function(sisaWaktu){
-        console.log(sisaWaktu)
-        if(sisaWaktu>0)
-        readBooksPromise(sisaWaktu, books[1].then(function(sisaWaktu){
-            if(sisaWaktu>0){
-                readBooksPromise(sisaWaktu, books[2].then(function(sisaWaktu){
-                    if(sisaWaktu!=0){
-                        console.log('sudah habis terbaca')
-                    }
-                }))
-            }
-        }))
-        .catch(function(sisaWaktu){
-            console.log(sisaWaktu)
-        })
-    }))
+function execute(time, ind, booksQueue){
+    readBooksPromise(time, books[ind])
+    .then(function(remainingTime){
+        time = remainingTime;
+        booksQueue = booksQueue - 1;
+        if(booksQueue>0){
+            execute(time, ind+1, booksQueue);
+        }
+    })
+    .catch(function(error){
+
+    });
+
 }
-bacaBuku();
+execute(time, 0, bookAntri);
+
+
+// function bacaBuku(){
+//     readBooksPromise(10000, books[0].then(function(sisaWaktu){
+//         console.log(sisaWaktu)
+//         if(sisaWaktu>0)
+//         readBooksPromise(sisaWaktu, books[1].then(function(sisaWaktu){
+//             if(sisaWaktu>0){
+//                 readBooksPromise(sisaWaktu, books[2].then(function(sisaWaktu){
+//                     if(sisaWaktu!=0){
+//                         console.log('sudah habis terbaca')
+//                     }
+//                 }))
+//             }
+//         }))
+//         .catch(function(sisaWaktu){
+//             console.log(sisaWaktu)
+//         })
+//     }))
+// }
+// bacaBuku();
+
